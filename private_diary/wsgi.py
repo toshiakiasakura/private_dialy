833063069ad753c7b2fe4666ae1acfdc29bcc14d
toshiakiasakura <aflu.blossompaws@gmail.com>
2020-07-19 22:37:46 +0900

    modified:   private_diary/wsgi.py

--- private_diary/wsgi.py
+++ private_diary/wsgi.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'private_diary.settings')
 
-application = Cling(get_wsgi_application()) 
+application = get_wsgi_application() 
